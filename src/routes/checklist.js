4be2416b32cc48150d560329e2843eefee055b65
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2021-01-08 14:27:06 -0500

    listing data from model
    so far user can:
    
    - post a new task
    -retrieve all tasks
    - retrieve task by id

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -4,25 +4,33 @@ const router = express.Router();
 
 const Checklist = require('../models/checklist');
 
-router.get('/', (req, res) => {
-    console.log('Ola, Mundo')
-    res.send();
+router.get('/', async (req, res) => {
+    try {
+        let checklists =  await Checklist.find({ });
+        res.status(200).json(checklists);
+    } catch (error) {
+        res.status(500).json(error)
+    }
 });
 
 router.post('/', async (req, res) => {
     let { name } = req.body
     
     try {
-        let checklist =  await Checklist.create({ name })
+        let checklist =  await Checklist.create({ name });
         res.status(200).json(checklist);
     } catch (error) {
         res.status(422).json(error)
     }
 });
 
-router.get('/:id', (req, res) => {
-    console.log(req.params.id)
-    res.send(`ID: ${req.params.id}`)
+router.get('/:id', async (req, res) => {
+    try {
+        let checklist =  await Checklist.findById(req.params.id);
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error)
+    }
 })
 
 router.put('/:id', (req, res) => {
